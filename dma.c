6c8f084f3eae59d0f94dfa58598d99c26557c755
Oishika Chaudhury <oc618@nyu.edu>
2020-05-29 16:48:27 +0530

    Changed findFit

--- dma.c
+++ dma.c
@@ -11,8 +11,10 @@ int MALLOC_NUM;
 int FREE_NUM;
 
 
-//int throughput(){
-//    return ALLOCATED/(sbrk(0)-HEADER);
+int throughput(){
+    int val = -(HEADER - sbrk(0));
+    printf("The number of requested bytes is %d, and total heap utilisation is %d\n", ALLOCATED, val);
+}
 
 /*Create the initial empty heap with a dummy header node, set values for global variables */
 void init(){
@@ -26,36 +28,44 @@ void init(){
 
 /*This function looks for a block which is large enough to hold our allocated block*/
 void* findFit(int size){
-    void* dummy = (int *)HEADER+3;
-    while (*(int *)dummy != 0){
+    void* dummy = *(long*)((int*)HEADER+3); //NOTE: (int*)HEADER+3 is not the address of the successor block, but the adress where this address is stored
+    while (dummy != 0){
          if ((*(unsigned int*)dummy & ~0x7) > size ) return dummy;
-         dummy = (unsigned int*)dummy + 3; //move up 12 bytes, i.e size bytes
+         dummy = *(long*)((int*)dummy+3); //dummy node is now equal to the successor node
     }
     return NULL;
 }
 
 void* mallocAttempt(int size){
-    void* val;
-    if (size < 24) size = 24;
-    else if (size % 24) size = (24*((size/24) + 1)); /*round up size to the nearest multiple of 24*/
-    val = findFit(size);
-    if (!(val)) val = sbrk(size);  /*We have found a free block large enough for our allocate request*/
-    *(unsigned int*)val = size | 0x1; //assign header
-    *((unsigned int*)val + size - 4) = size | 0x1;
-    MALLOC_NUM += 1;
     ALLOCATED += size;
+    void* val;
+    //1. Adjust size to make sure it is rounded up to the nearest multiple of 24
+    if (size < 24) size = 24;
+    else if (size % 24) size = (24*((size/24) + 1));
+    //2. Look for size bytes in the free blocks
+    val = findFit(size);
+    //3. Otherwise request for more memory from heap
+    if (!(val)) val = sbrk(size);
+    *(unsigned int*)val = size | 0x1; //assign header
+    *(int *)(((char*)val + size - 4)) = size | 0x1;
+    MALLOC_NUM += 1;
     return (unsigned int*)val + 4;
 }
 
 int main() {
     init();
-    printf("Printing values after initiliasing the allocator.\n");
+    printf("Printing values after initiliasing the allocator. \n");
     printf("HEADER : %d CURR_TAIL: %d\n", HEADER, CURR_TAIL);
     printf("size: %d predecessor: %d successor: %d \n", *(int*)HEADER, *((long *)((unsigned int*)CURR_TAIL + 1)),*((long *)((unsigned int*)CURR_TAIL + 1) + 1));
     printf("----------------------------------------------------------\n");
-    printf("Malloc attempt 1: \n");
+    printf("MALLOC ATTEMPT 1 \n");
     int* new = (int *)mallocAttempt(32);
     *new = 42;
-    printf("Value stored at new is: %d and the size of the requested block is: %d \n", *new, *(new-4));
-    
+    printf("Value stored at new is: %d and the size of the requested block is: %d, %d\n", *new, *(new-4)-1, *(int *)(((char*)(new-4) + (*(new-4)-1) - 4)));
+    printf("MALLOC ATTEMPT 2 \n");
+    int* new2 = (int *)mallocAttempt(14);
+    *new2 = 55;
+    printf("Value stored at new is: %d and the size of the requested block is: %d %d\n", *new2, *(new2-4)-1, *(int *)(((char*)(new2-4) + (*(new2-4)-1) - 4)));
+    printf("PRINTING THROUGHPUT\n");
+    throughput();
 }
